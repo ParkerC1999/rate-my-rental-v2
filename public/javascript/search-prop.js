22689b3f960a1a0de6db5dbb6f35dea781edfa12
Brandon Evans <brandon.evans0708@gmail.com>
2021-08-07 20:13:11 -0600

    added more files and modified some things

---
+++ public/javascript/search-prop.js
@@ -0,0 +1,23 @@
+async function newPropSearch(event) {
+   event.preventDefault();
+
+   const response = await fetch(`/api/properties`, {
+      method: 'GET',
+      body: JSON.stringify({
+         
+         
+      }),
+      headers: {
+         'Content-Type': 'application/json'
+      }
+   });
+
+   if (response.ok) {
+      document.location.replace('/properties');
+   } else {
+      alert(response.statusText);
+   }
+}
+
+document.querySelector('.propBtn').addEventListener('click', newPropSearch);
+console.log(newPropSearch)
--- server.js
+++ server.js
@@ -25,16 +25,16 @@ const helpers = require('./utils/helper');
 
 const hbs = exphbs.create({ helpers });
 
+app.get('/', (req, res) => res.render('homepage'));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.get('/', (req, res) => res.render('homepage'));
 
 // app.use(require('./controllers/'));
 
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ force: false }).then(() => {
    app.listen(PORT, () => console.log('Now listening on PORT ' + PORT));
 });
